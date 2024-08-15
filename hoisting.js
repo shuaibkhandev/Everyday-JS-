f117742353f4ad8fd4409adb38544bea8d7c60ff
Shuaib Khan <shuaib.middle@gmail.com>
2024-08-15 10:25:37 +0500

    Hositng Practice

---
+++ hoisting.js
@@ -0,0 +1,16 @@
+
+
+// Hositing Practice
+
+checkProfession()
+
+console.log(`I am a ${profession}`);
+var profession = "Web Developer";
+console.log(`I am a ${profession}`);
+
+function checkProfession() {
+  console.log(`I am a ${profession}`);
+  var profession = "Full Stack Developer";
+  console.log(`I am a ${profession}`);
+}
+
